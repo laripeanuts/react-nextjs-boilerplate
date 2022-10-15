88d4a79a559422d97ab05c62bf440ce8cb221d20
laripeanuts <larissarabelolf@gmail.com>
2022-10-14 22:53:08 -0300

    build: configura commitlint

--- next.config.js
+++ next.config.js
@@ -8,17 +8,10 @@ const nextConfig = {
 module.exports = nextConfig;
 
 // const withPWA = require("next-pwa");
-// const runtimeCaching = require("next-pwa/cache");
 
 // module.exports = withPWA({
-//   runtimeCaching,
-//   swcMinify: true,
-//   experimental: {
-//     // Enables the styled-components SWC transform
-//     styledComponents: true
-//   },
 //   pwa: {
 //     dest: "public",
-//     disable: process.env.NODE_ENV === "development"
+//     disable: process.env.NODE_ENV !== "development"
 //   }
 // });
